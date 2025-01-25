3a354caf8cdc781883c47b9d64e18f45576d6eaf
adebisivince <adesanmiadebisi@gmail.com>
2025-01-25 10:52:27 +0000

    refac: request module refractored

--- modules_example/request.js
+++ modules_example/request.js
@@ -1,13 +1,14 @@
+module.exports.REQUEST_TIMEOUT = 500;
 function encrypt(data){
     return 'encrypted data'
 }
 
-function send(url, data){
+module.exports.send = function send(url, data){
     const encryptedData = encrypt(data);
     console.log(`sending ${encryptedData} to ${url}`);
 
 }
 
-module.exports ={
-    send
-}
+// module.exports ={
+//     send
+// }
