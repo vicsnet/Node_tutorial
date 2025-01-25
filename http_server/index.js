e10e76811d843e0f3632b3163cf55609d18963be
adebisivince <adesanmiadebisi@gmail.com>
2025-01-25 20:43:19 +0000

    refrac: parameterised rls

--- http_server/index.js
+++ http_server/index.js
@@ -1,8 +1,39 @@
 const http = require('http');
 
 const PORT = 3000;
-const server = http.createServer((req, res) =>{
-    if(req.url === '/friends'){
+const server = http.createServer();
+
+const friends = [
+    {
+        id:0,
+        name: 'Nikol Tesla'
+    },
+    {
+        id:1,
+        name: 'Albert Einstein'
+    },
+    {
+        id:2,
+        name: 'Sir Isaac Netwon'
+    },
+]
+
+server.on('request',(req, res) =>{
+    // splitting url
+    const items = req.url.split('/');
+    if(req.method === 'POST' & items[1] === 'friends'){
+        req.on('data', (data) =>{
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friends.push(JSON.parse(friend));
+            
+        });
+        req.pipe(res );
+
+    }
+    else if(req.method  === 'GET' && items[1] === 'friends'){
+
+    // if(req.url === '/friends'){
 
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
@@ -11,12 +42,22 @@ const server = http.createServer((req, res) =>{
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         // res.end('Hello! Sir Isaac Newton is your friend')
-        res.end(JSON.stringify({
-            id: 1,
-            name: 'Sir Isac Newton'
-        }));
+        if  (items.length === 3){
+            const friendIndex = Number(items[2])
+            res.end(JSON.stringify(friends[friendIndex]));
+        }else{
+
+            res.end(JSON.stringify(
+            //     {
+            //     id: 1,
+            //     name: 'Sir Isac Newton'
+            // }
+                friends
+        ));
+        }
     }
-    else if (req.url === '/messages'){
+    else if (items[1] === 'messages'){
+    // else if (req.url === '/messages'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
