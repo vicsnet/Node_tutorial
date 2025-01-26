f7eadc434e093531f2f1eae99ebdc8f9b8c1b560
adebisivince <adesanmiadebisi@gmail.com>
2025-01-26 21:57:44 +0000

    feat: Express project created

---
+++ express-project/server.js
@@ -0,0 +1,50 @@
+const express = require('express');;
+
+const app = express();
+
+const PORT = 3000;
+const friends =[{
+    id: 0,
+    name: 'Albert Einstein'
+},
+{
+    id: 1,
+    name: 'Sir Isaac Mewton'
+}]
+
+app.get('/',(req, res)=>{
+    res.send({
+        id:1,
+        name:'Vincent'
+    })
+})
+
+app.get('/friends',(req, res)=>{
+    res.json(friends)
+})
+
+app.get('/friends/:friendId',(req, res)=>{
+    const friendId =Number(req.params.friendId);
+    const friend = friends[friendId];
+    if(friend){
+        res.json(friends)
+    }else{
+
+        res.status(404).json({
+            error:'User not found'
+        })
+    }
+
+})
+
+app.get('/messages', (req, res) =>{
+    res.send('<ul><li>Hello Albert</li></ul>')
+});
+app.post('/messages', (req, res) =>{
+    console.log('Updating messages ...');
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log('Listening on', PORT, '...')
+})
