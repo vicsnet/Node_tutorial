75ec2e1b42c648eb45c67615896011911e1a5c70
adebisivince <adesanmiadebisi@gmail.com>
2025-01-30 12:55:36 +0000

    feat: automating the whole fnasa production project

--- nasa_project/server/src/app.js
+++ nasa_project/server/src/app.js
@@ -1,5 +1,8 @@
+const path = require('path')
 const express = require('express');
 const cors = require('cors')
+
+
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -9,6 +12,12 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
